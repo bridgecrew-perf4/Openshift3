86591cab5bc8fb6620f0279afbef6b146fad8feb
Gary Crowe <gary.crowe@computacenter.com>
2021-01-26 19:12:16 +0000

    Working. Needs code for memory & cpu extraction from tuple

--- KVM/staticip.tf
+++ KVM/staticip.tf
@@ -2,11 +2,14 @@
 variable "host_names"  { default = ["neo", "trinity", "morpheus"] }
 variable "domain"      { default = "OCP3.local" }
 variable "ip_type"     { default = "static" } # dhcp is other valid type
-variable "memoryMB"    { default = [1024*16, 1024*8, 1024*8] }
-variable "cpu"         { default = [4,2,2] }
+variable "memoryMB"    { default = 1024*16 }
+variable "cpu"         { default = 2 }
 variable "prefixIP"    { default = "192.168.200" }
-variable "octetIP"     { default = [70, 80, 90] }
+variable "octetIP"     { default = ["70", "80", "90"] }
 variable "DockerBytes" { default = 1024*1024*1024*30 }
+
+#variable "cpu"        { default = [4,2,2] }
+#variable "memoryMB"    { default = [1024*16, 1024*8, 1024*8] }
 
 terraform {
   required_providers {
@@ -26,8 +29,7 @@ provider "libvirt" {
 resource "libvirt_volume" "os_image" {
   count = length(var.host_names)
   name  = "${var.host_names[count.index]}-os_image"
-# name = "${var.hostname}-os_image"
-  pool = "default"
+  pool  = "default"
   source = "/var/lib/libvirt/images/rhel-server-7.8-big-x86_64-kvm.qcow2"
   format = "qcow2"
 }
@@ -36,36 +38,34 @@ resource "libvirt_volume" "os_image" {
 resource "libvirt_cloudinit_disk" "commoninit" {
           count = length(var.host_names)
           name  = "${var.host_names[count.index]}-commoninit.iso"
-#         name = "${var.hostname}-commoninit.iso"
           pool = "default"
-          user_data = data.template_file.user_data.rendered[count]
-          meta_data = data.template_file.meta_data.rendered[count]
+          user_data = data.template_file.user_data.*.rendered[count.index]
+          meta_data = data.template_file.meta_data.*.rendered[count.index]
 }
 
-
+# Render cloud init
 data "template_file" "user_data" {
   count = length(var.host_names)
   template = file("${path.module}/cloud_init.cfg")
   vars = {
     hostname = var.host_names[count.index]
-    fqdn = "${var.host_names[count.index]}.${var.domain}"
-
-#   hostname = var.hostname
-#   fqdn = "${var.hostname}.${var.domain}"
+    fqdn = var.domain
   }
 }
 
+# Render networking
 data "template_file" "meta_data" {
-  count = length(var.host_names)
   template = file("${path.module}/network_config_${var.ip_type}.cfg")
+  count = length(var.octetIP)
   vars = {
     domain = var.domain
     prefixIP = var.prefixIP
     LastOCT  = var.octetIP[count.index]
   }
+
 }
 
-# Extra data disk for Docker
+# Render extra data disk for Docker
 resource "libvirt_volume" "disk_data1" {
   count = length(var.host_names)
   name           = "${var.host_names[count.index]}-docker"
@@ -74,23 +74,25 @@ resource "libvirt_volume" "disk_data1" {
   format         = "qcow2"
 }
 
-
 # Create the machine
 resource "libvirt_domain" "domain-RHEL" {
   # domain name in libvirt, not hostname
   count = length(var.host_names)
   name = "${var.host_names[count.index]}-${var.domain}"
-  memory = var.memoryMB[count]
-  vcpu = var.cpu[count]
+  memory = var.memoryMB
+  vcpu = var.cpu
 
-  disk { volume_id = libvirt_volume.os_image.id[count]   }
-  disk { volume_id = libvirt_volume.disk_data1.id[count] }
+#  memory = var.memoryMB[count,index]
+#  vcpu = var.cpu[count,index]
+
+  disk { volume_id = libvirt_volume.os_image[count.index].id   }
+  disk { volume_id = libvirt_volume.disk_data1[count.index].id }
 
   network_interface {
        network_name = "General"
   }
 
-  cloudinit = libvirt_cloudinit_disk.commoninit.id[count]
+  cloudinit = libvirt_cloudinit_disk.commoninit[count.index].id
 
   # IMPORTANT
   # RHEL can hang is a isa-serial is not present at boot time.
